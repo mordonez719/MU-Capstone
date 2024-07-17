8d6e1c50ccb3e3bc46f99401611b4222398680ea
Michelle Johanna Ordonez <mordonezcampos@meta.com>
2024-07-17 16:45:21 -0700

    save meal filters

--- front-end/src/pages/main/meals/components/MealResults.jsx
+++ front-end/src/pages/main/meals/components/MealResults.jsx
@@ -48,7 +48,7 @@ function MealResults(props){
     }
 
     // updates the user's meal search history
-    const updateHistory = async (query) => {
+    const updateHistory = async (query, diets, healths, types, min, max) => {
         fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/search/meal`,
         {
             method: "PUT",
@@ -56,7 +56,12 @@ function MealResults(props){
                 "Content-Type": "application/json",
                 },
             body: JSON.stringify({
-                "newSearch": query
+                "newSearch": query,
+                "newDiets": diets,
+                "newHealths": healths,
+                "newTypes": types,
+                "min": min,
+                "max": max
                 }),
         }
         )
@@ -67,22 +72,28 @@ function MealResults(props){
     };
 
     const handleSearch = () => {
-        let filters = ''; // empty string for optional filters
+        let filters = ''; // empty string for fetching by optional filters
+        
+        let filterHistory = {}; // empty map to sort entered filters to add to history
+        filterHistory["diet"] = [];
+        filterHistory["health"] = [];
+        filterHistory["dishType"] = [];
 
-        // iterates through filter map, adding filters to the string if they were checked off
+        // iterates through filter map, adding filters to the string and map if they were checked off
         for (var index in filterMap) {
             var mapKey = index;
             if (filterMap[mapKey][1]) { // checked off?
                 let filtString = `&${filterMap[mapKey][0]}=${mapKey}` // creates a string of the applicable filter
                 filtString = filtString.replace(" ", "%20") // formats for spaces
                 filters = filters.concat(filtString) // adds the new string to the aggregate filters string
+                
+                filterHistory[filterMap[mapKey][0]].push(mapKey) // adds filter to map to store in history
             }
         }
 
         let calories = `&calories=${minCal}-${maxCal}` // formats calorie variables for fetching
-
         fetchData(searchQuery, filters, calories); // passes the types queries and selected filters for fetching
-        updateHistory(searchQuery) // adds search query to user's history
+        updateHistory(searchQuery, filterHistory["diet"], filterHistory["health"], filterHistory["dishType"], minCal, maxCal) // adds search query and filters to user's history
     }
 
     // empty array for meal displays
