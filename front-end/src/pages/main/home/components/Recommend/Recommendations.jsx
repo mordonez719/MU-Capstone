74bc419f6696bb6947cd061b63cb2125944bcc0b
Michelle Johanna Ordonez <mordonezcampos@meta.com>
2024-07-17 18:48:05 -0700

    format filters for search

--- front-end/src/pages/main/home/components/Recommend/Recommendations.jsx
+++ front-end/src/pages/main/home/components/Recommend/Recommendations.jsx
@@ -15,31 +15,26 @@ import { useState, useEffect } from 'react'
 
 function Recommendations(props) {
     const [history, setHistory] = useState([])
+    const [exTypes, setExTypes] = useState([])
+    const [muscles, setMuscles] = useState([])
+    const [difficulties, setDiffs] = useState([])
+
     const user = props.user
 
     // get search history
     const fetchHistory = async () => {
-        const response = await fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/history/meal`,
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/user/data`,
         {
-            method: "POST",
+            method: "GET",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({
-                username: user
-            }),
         })
         const data = await response.json()
-        setHistory(data)
-        // console.log(history)
-        // if (history) {
-        //     // console.log("true")
-        //     let words = '';
-        //     for (const word in history){
-        //         words.concat(`rel_trg=${word}`)
-        //         console.log(words)
-        //         }
-        // }
+        setHistory(data.exSearches)
+        setExTypes(data.exTypes)
+        setMuscles(data.muscles)
+        setDiffs(data.difficulties)
     };
 
     useEffect(() => {
@@ -47,34 +42,38 @@ function Recommendations(props) {
       },[])
 
     useEffect(() => {
-        console.log(history)
         if (history) {
-            console.log("true")
             let words = `rel_trg=${history[0]}`;
             for (let i = 1; i < history.length; i++){
-                console.log(history[i])
+                if (history[i]){
                 words = words.concat(`&rel_trg=${history[i]}`)
-                console.log(words)
-                }
+                }}
             fetchData(words)
         }
     }, [history])
 
     // TODO: call top words api
     const fetchData = async (words) => {
-        const baseURL = `https://api.datamuse.com/words?${words}`        
-
+        const baseURL = `https://api.datamuse.com/words?${words}`  
         const response = await fetch(baseURL);
         const data = await response.json();
-
-        console.log(data)
     }
 
-    // TODO: print top relevant search terms
+    // reduces an array to hold each value once, sorted by frequency
+    function reduceSort(array) {
+        const frequencyMap = {};
+        array.forEach(filter => {
+            if (!frequencyMap[filter]) {
+                frequencyMap[filter] = 1;
+            } else {
+                frequencyMap[filter]++;
+            }
+        });
+    }
     
     return (
         <>
-        <div id="recommendation-container"> R
+        <div id="recommendation-container"> Recommendations Here
         </div>
         </>
     )
