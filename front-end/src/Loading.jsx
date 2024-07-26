fdee6bd10d43f93c694fbdaee940f26cf315520c
Michelle Johanna Ordonez <mordonezcampos@meta.com>
2024-07-26 12:36:43 -0700

    loader created

---
+++ front-end/src/Loading.jsx
@@ -0,0 +1,23 @@
+/*
+
+Loading.jsx
+
+Creates an animated loading spinner.
+
+Calls: 
+Called In:
+
+*/
+
+import './Loading.css'
+
+function Loading(){
+    return (
+        <>
+        <div className='loader'>
+        </div>
+        </>
+    )
+}
+
+export default Loading
